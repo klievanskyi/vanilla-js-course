d56fe3f365aef5d4e7f2f37825c11534c1557a08
NickoKlevanski <nikolai.klevanski@gmail.com>
2019-10-23 18:18:00 +0300

    upd words count solution

--- 2-character-count/js/app-word-count.js
+++ 2-character-count/js/app-word-count.js
@@ -1,15 +1,23 @@
 const textArea = document.querySelector('#text');
-const characterCount = document.querySelector('#character-count');
-const wordCount = document.querySelector('#word-count');
+const charOutput = document.querySelector('#character-count');
+const wordOutput = document.querySelector('#word-count');
 
-const getAreaValue = input => input.value;
+const getCharCount = input => input.value.length;
+
+const getWordCount = input => {
+  if (input.value.length === 0) return 0;
+  else {
+    return input.value.split(" ").filter(val => val !=="").length;
+  }
+};
 
 const showCount = (value, output) => output.textContent = value;
 
 const handleInput = () => {
-  const value = getAreaValue(textArea);
-  showCount(value.split(' ').length, wordCount);
-  showCount(value.length, characterCount);
+  const charCount = getCharCount(textArea);
+  const wordCount = getWordCount(textArea);
+  showCount(wordCount, wordOutput);
+  showCount(charCount, charOutput);
 }
 
 textArea.addEventListener('input', handleInput, false);
