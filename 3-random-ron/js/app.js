0e56a2a718194e6789aa4c4a9a18399ffc1c9a1d
NickoKlevanski <nikolai.klevanski@gmail.com>
2019-10-29 23:45:13 +0200

    add without duplicates upd

--- 3-random-ron/js/app.js
+++ 3-random-ron/js/app.js
@@ -1,6 +1,7 @@
 const btn = document.querySelector('button');
 const quote = document.querySelector('blockquote');
 const endPoint = 'http://ron-swanson-quotes.herokuapp.com/v2/quotes';
+const existQuotesArray = [];
 
 const getQuote = () => {
   fetch(endPoint)
@@ -12,7 +13,14 @@ const getQuote = () => {
       }
     })
     .then(data => {
-      quote.textContent = data;
+      console.log(existQuotesArray.indexOf(data));
+      if (existQuotesArray.indexOf(data) == -1) {
+        existQuotesArray.push(data);
+        quote.textContent = data;
+        console.log(existQuotesArray);
+      } else {
+        getQuote();
+      }
     })
     .catch(err => {
       console.warn('Something went wrong.', err);
