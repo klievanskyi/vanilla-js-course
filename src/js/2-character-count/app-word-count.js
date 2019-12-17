b9310d088c0a63947e3d1b3321c9eceab129a0ee
NickoKlevanski <nikolai.klevanski@gmail.com>
2019-12-17 18:34:53 +0200

    add test main page

--- src/js/2-character-count/app-word-count.js
+++ src/js/2-character-count/app-word-count.js
@@ -1,21 +1,23 @@
-const textArea = document.querySelector('#text');
-const charOutput = document.querySelector('#character-count');
-const wordOutput = document.querySelector('#word-count');
+if (document.location.pathname === '/word-count.html') {
+  const textArea = document.querySelector('#text');
+  const charOutput = document.querySelector('#character-count');
+  const wordOutput = document.querySelector('#word-count');
 
-const getCharCount = input => input.value.length;
+  const getCharCount = input => input.value.length;
 
-const getWordCount = input => {
-  if (input.value.length === 0) return 0;
-  return input.value.split(' ').filter(val => val !== '').length;
-};
+  const getWordCount = input => {
+    if (input.value.length === 0) return 0;
+    return input.value.split(' ').filter(val => val !== '').length;
+  };
 
-const showCount = (value, output) => (output.textContent = value);
+  const showCount = (value, output) => (output.textContent = value);
 
-const handleInput = () => {
-  const charCount = getCharCount(textArea);
-  const wordCount = getWordCount(textArea);
-  showCount(wordCount, wordOutput);
-  showCount(charCount, charOutput);
-};
+  const handleInput = () => {
+    const charCount = getCharCount(textArea);
+    const wordCount = getWordCount(textArea);
+    showCount(wordCount, wordOutput);
+    showCount(charCount, charOutput);
+  };
 
-textArea.addEventListener('input', handleInput, false);
+  textArea.addEventListener('input', handleInput, false);
+}
--- src/js/3-random-ron/app-random-ron.js
+++ src/js/3-random-ron/app-random-ron.js
@@ -1,31 +1,32 @@
-const btn = document.querySelector('button');
-const quote = document.querySelector('blockquote');
-const endPoint = 'http://ron-swanson-quotes.herokuapp.com/v2/quotes';
-const existQuotesArray = [];
+if (document.location.pathname === '/random-ron.html') {
+  const btn = document.querySelector('button');
+  const quote = document.querySelector('blockquote');
+  const endPoint = 'http://ron-swanson-quotes.herokuapp.com/v2/quotes';
+  const existQuotesArray = [];
 
-const getQuote = () => {
-  fetch(endPoint)
-    .then(resp => {
-      if (resp.ok) {
-        return resp.json();
-      } else {
+  const getQuote = () => {
+    fetch(endPoint)
+      .then(resp => {
+        if (resp.ok) {
+          return resp.json();
+        }
         return Promise.reject(resp);
-      }
-    })
-    .then(data => {
-      console.log(existQuotesArray.indexOf(data));
-      if (existQuotesArray.indexOf(data) == -1) {
-        existQuotesArray.push(data);
-        quote.textContent = data;
-        console.log(existQuotesArray);
-      } else {
-        getQuote();
-      }
-    })
-    .catch(err => {
-      console.warn('Something went wrong.', err);
-      quote.textContent = 'Something went wrong.';
-    });
-};
+      })
+      .then(data => {
+        console.log(existQuotesArray.indexOf(data));
+        if (existQuotesArray.indexOf(data) == -1) {
+          existQuotesArray.push(data);
+          quote.textContent = data;
+          console.log(existQuotesArray);
+        } else {
+          getQuote();
+        }
+      })
+      .catch(err => {
+        console.warn('Something went wrong.', err);
+        quote.textContent = 'Something went wrong.';
+      });
+  };
 
-btn.addEventListener('click', getQuote, false);
+  btn.addEventListener('click', getQuote, false);
+}
