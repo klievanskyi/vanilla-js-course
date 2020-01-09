3c176b7c34df346178cb7443a50e315eac879904
NickoKlevanski <nikolai.klevanski@gmail.com>
2020-01-09 18:43:23 +0200

    NYTop Stories first part

--- src/js/3-random-ron/app-random-ron.js
+++ src/js/3-random-ron/app-random-ron.js
@@ -7,6 +7,7 @@ if (document.location.pathname === '/random-ron.html') {
   const getQuote = () => {
     fetch(endPoint)
       .then(resp => {
+        console.log(resp.json());
         if (resp.ok) {
           return resp.json();
         }
--- src/js/4-NYT-top-stories/app-nyt-top-stories.js
+++ src/js/4-NYT-top-stories/app-nyt-top-stories.js
@@ -1,17 +1,47 @@
 if (document.location.pathname === '/nyt-top-stories.html') {
   const endPoint =
     'https://api.nytimes.com/svc/topstories/v2/home.json?api-key=EdS4J6tNjJTENkNBtxKoWCMTHGdZJNdQ';
+  const resultsArray = [];
+  const list = document.querySelector('#list');
 
-  const getStories = () =>
-    fetch(endPoint).then(resp => {
-      if (resp.ok) {
-        return resp.json();
-      }
-      return Promise.reject(resp);
-    });
-  // .then(data => {
-
-  // });
+  const getStories = () => {
+    fetch(endPoint)
+      .then(resp => {
+        if (resp.ok) {
+          return resp.json();
+        }
+        return Promise.reject(resp);
+      })
+      .then(data => {
+        console.log(data.results);
+        data.results.map(elem => {
+          resultsArray.push(elem);
+        });
+        list.innerHTML = resultsArray
+          .map(elem => {
+            return `
+            <article class="flex flex-col max-w-md mx-3">
+              <img src="https://via.placeholder.com/150" alt="" />
+              <h2 class="font-semibold py-2">
+                <a href="${elem.url}">${elem.title}</a>
+              </h2>
+              <span>${elem.abstract}</span>
+              <span class="py-2">
+                <a href="${elem.short_url}">Read more</a>
+              </span>
+            </article>
+            `;
+          })
+          .join('');
+      });
+  };
   getStories();
-  console.log('asd');
+  // const template = (elem) => {
+
+  // }
+  // list.innerHTML = resultsArray
+  //   .map(elem => {
+  //     return `<li>${elem.abstract}</li>`;
+  //   })
+  //   .join('');
 }
