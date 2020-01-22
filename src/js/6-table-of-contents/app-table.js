bb09c85b5936dea899df714a71bd486b62bc657b
NickoKlevanski <nikolai.klevanski@gmail.com>
2020-01-22 18:44:27 +0200

    table of contents: add array of titles

--- src/js/6-table-of-contents/app-table.js
+++ src/js/6-table-of-contents/app-table.js
@@ -1,2 +1,11 @@
 if (document.location.pathname === '/table-of-contents.html') {
+  const entryTable = document.querySelector('#table-of-contents');
+
+  const titlesArray = () => {
+    const array = [...document.querySelectorAll('h2')];
+    const contextArray = array.map(element => `#${element.textContent}`);
+    return contextArray;
+  };
+
+  console.log(titlesArray());
 }
