a58d3128a9da0c6df894da418f9266473173f106
NickoKlevanski <nikolai.klevanski@gmail.com>
2020-01-24 17:31:43 +0200

    table of contents: add part with missing IDs

--- src/js/6-table-of-contents/app-table.js
+++ src/js/6-table-of-contents/app-table.js
@@ -3,6 +3,12 @@ if (document.location.pathname === '/table-of-contents.html') {
   const arrayHeaders = [...document.querySelectorAll('h2')];
 
   const templateCell = array => {
+    array.map(elem => {
+      if (elem.id.length === 0) {
+        const str = elem.textContent.replace(/[^a-z0-9]+/gi, '-').toLowerCase();
+        elem.setAttribute('id', str);
+      }
+    });
     return array
       .map(elem => `<div><a href="#${elem.id}">${elem.textContent}</a></div>`)
       .join('');
